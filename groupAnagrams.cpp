6e319075837e1a865e81136604a5a00e3dd500cb
Gabriel Blanco Gutiérrez <gabriel.blanco@utec.edu.pe>
2024-01-17 11:36:43 -0500

    Ej2

--- groupAnagrams.cpp
+++ groupAnagrams.cpp
@@ -2,13 +2,24 @@
 #include <vector>
 #include <unordered_map>
 #include <cassert>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // add your code
-    }
+        unordered_map<string, vector<string>> m;
+		for(string s : strs){
+			string t = s;
+			sort(t.begin(), t.end());
+			m[t].push_back(s);
+		}
+		vector<vector<string>> ans;
+		for(auto p : m){
+			ans.push_back(p.second);
+		}
+		return ans;
+    };
 };
 
 void test(){
@@ -23,6 +34,7 @@ void test(){
 	strs = {"a"};
 	ans = {{"a"}};
 	assert(Solution().groupAnagrams(strs) == ans);
+
 }
 
 int main(){
