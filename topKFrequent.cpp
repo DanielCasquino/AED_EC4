440270eb83e02e30e95f91fe8262b563a60630a4
Daniel Casquino <daniel.casquino@utec.edu.pe>
2024-01-17 11:57:08 -0500

    Largest k pending in 1st

--- topKFrequent.cpp
+++ topKFrequent.cpp
@@ -3,35 +3,49 @@
 #include <vector>
 #include <unordered_map>
 #include <cassert>
+#include <queue>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        // add your code
-		
-    }
+	vector<int> topKFrequent(vector<int> &nums, int k)
+	{
+		unordered_map<int, int> mapita;
+		for (auto n : nums) // O(n)
+		{
+			mapita[n]++;
+		}
+		cout << "content:\n";
+		for (auto pair : mapita)
+		{
+			cout << pair.first << ": " << pair.second << endl;
+		}
+		vector<int> output;
+		return output;
+	}
 };
 
-
-void test(){
-	vector<int> nums = {1,1,1,2,2,3};
-	vector<int> ans = {1,2};
+void test()
+{
+	vector<int> nums = {1, 1, 1, 2, 2, 3};
+	vector<int> ans = {1, 2};
 	assert(Solution().topKFrequent(nums, 2) == ans);
-	
+
 	nums = {1};
 	ans = {1};
 	assert(Solution().topKFrequent(nums, 1) == ans);
-	
-	nums = {1,1,4,4,2,4,3,3,3,3};
-	ans = {3,4};
+
+	nums = {1, 1, 4, 4, 2, 4, 3, 3, 3, 3};
+	ans = {3, 4};
 	assert(Solution().topKFrequent(nums, 2) == ans);
-	
-	nums = {1,7,4,4,1,4,3,7,3,3,1};
-	ans = {3,4,1};
+
+	nums = {1, 7, 4, 4, 1, 4, 3, 7, 3, 3, 1};
+	ans = {3, 4, 1};
 	assert(Solution().topKFrequent(nums, 3) == ans);
 }
 
-int main(){
+int main()
+{
 	test();
 	return 0;
 }
