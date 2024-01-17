a3742914821795c452868bfbb3939c0d1bdc177e
Alexis Raza <gilver.raza@utec.edu.pe>
2024-01-17 12:39:00 -0500

    validadorDeSudoku

--- isValidSudoku.cpp
+++ isValidSudoku.cpp
@@ -6,9 +6,55 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // add your code
-        
-    }
+			const int n = 9;
+			// validamos las filass para verificar repeticiones
+			for (int i = 0; i < n; ++i) {
+					unordered_map<char, int> rows;
+					for (int j = 0; j < n; ++j) {
+							char num = board[i][j];
+							if (num != '.') {
+									if (rows.find(num) != rows.end()) {
+											return false;  // carácter repetido en la misma fila
+									} rows[num] = 1;
+							}
+					}
+			}
+
+			// validamos las columnas para verificar repeticiones
+			for (int j = 0; j < n; ++j) {
+					unordered_map<char, int> cols;
+					for (int i = 0; i < n; ++i) {
+							char num = board[i][j];
+							if (num != '.') {
+									if (cols.find(num) != cols.end()) {
+											return false;  // carácter repetido en la misma columna
+									}
+									cols[num] = 1;
+							}
+					}
+			}
+
+			// Recorrer las casillas de 3x3 para verificar repeticiones
+			const int casillasSize = 3;
+			for (int i = 0; i < n; i += casillasSize) {
+					for (int j = 0; j < n; j += casillasSize) {
+							unordered_map<char, int> casillas;
+							for (int x = i; x < i + casillasSize; ++x) { // + 3 ya que son bloques de 3x3 y hay 9 bloques
+									for (int y = j; y < j + casillasSize; ++y) {
+											char num = board[x][y];
+											if (num != '.') {
+													if (casillas.find(num) != casillas.end()) {
+															return false;  // caracter repetido en la misma casilla de 3x3
+													}
+													casillas[num] = 1;
+											}
+									}
+							}
+					}
+			}
+
+    return true;  // sudoku válidado
+  }
 };
 
 void test(){
